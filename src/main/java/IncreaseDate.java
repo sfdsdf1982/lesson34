52f27779d8865956873ce29bf8a5336d5d616dd0
proger164 <gerasimenkosv@bk.ru>
2023-05-20 11:07:00 +0300

    Первая версия

---
+++ src/main/java/IncreaseDate.java
@@ -0,0 +1,13 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class IncreaseDate {
+    public static void main(String[] args) {
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DATE,3);
+        date = calendar.getTime();
+        System.out.println(date);
+    }
+}
